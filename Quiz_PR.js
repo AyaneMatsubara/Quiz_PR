589566a4a1bc25322d14bf27d4ef872c66dfd7f1
AyaneMatsubara <ayataka912nerv@gmail.com>
2018-09-09 01:49:07 +0900

    0908

--- Quiz_PR.js
+++ Quiz_PR.js
@@ -23,3 +23,14 @@ $(function() {
     	return false;
   	});
 });
+
+$(function(){	// #で始まるリンクをクリックしたら実行されます 
+	$('a[href^="#"]').click(function() {
+    	var speed = 750; // ミリ秒で記述
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $('body,html').animate({scrollTop:position}, speed, 'swing');
+        return false;
+    });
+});
